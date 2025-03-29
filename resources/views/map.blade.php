b963f714700b38b0eb16330bbb5f50ba12440402
Nicolas Lagrange <130107205+Grimkujow@users.noreply.github.com>
2025-03-29 16:36:29 +0100

    MAP RESTRICTION FINI
    zoom + delimitation + couleur map + control

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,29 +6,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>City Flow - Plateforme de Gestion de Ville Intelligente</title>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <link rel="icon" href="{{ asset("./assets/logo2.png") }}">
+  <link rel="icon" href="{{ asset('./assets/logo2.png') }}">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <title>Simple Marker</title>
-  <!-- The callback parameter is required, so we use console.debug as a noop -->
   <script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCucPKHUjomqAvneDLQlnzZuNZZHktt6_U&callback=console.debug&libraries=maps,marker&v=beta">
-    </script>
-  <style>
-    /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-    gmp-map {
-      height: 100%;
-    }
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCucPKHUjomqAvneDLQlnzZuNZZHktt6_U&callback=initMap&libraries=maps,marker&v=beta"></script>
 
+  <style>
     /* Optional: Makes the sample page fill the window. */
-    html,
     body {
       height: 100%;
       margin: 0;
       padding: 0;
     }
 
-    #google-map {
+    #map {
       border: 1px;
       width: 50vw;
       height: 85vh;
@@ -49,7 +41,6 @@
 
     .card-L {
       background-color: #1c78c9;
-
     }
 
     .card-S {
@@ -61,14 +52,136 @@
 <body>
   <div class="container">
     <div class="card card-L">
-      <div id="google-map">
-        <iframe src="https://snazzymaps.com/embed/694751" width="750px" height="600px" style="border:none;"></iframe>
-      </div>
+      <div id="map"></div>
     </div>
     <div class="card card-S">
       partie info
     </div>
   </div>
+
+  <!-- Script pour initialiser la carte -->
+  <script>
+    let map;
+
+    // S'assurer que la fonction initMap est appelée une fois que le DOM est entièrement chargé
+    function initMap() {
+      console.log("La fonction initMap est appelée !");  // Vérification dans la console
+
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 49.03485870361328, lng: 2.070168972015381 },
+        minZoom: 14,
+        maxZoom: 19,
+        zoom: 15,
+        mapTypeControl: false,
+        streetViewControl: false,
+        styles: [
+          {
+            "featureType": "landscape.natural",
+            "elementType": "geometry.fill",
+            "stylers": [
+              {
+                "visibility": "on"
+              },
+              {
+                "color": "#e0efef"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+              {
+                "visibility": "on"
+              },
+              {
+                "hue": "#1900ff"
+              },
+              {
+                "color": "#c0e8e8"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "lightness": 100
+              },
+              {
+                "visibility": "simplified"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "visibility": "on"
+              },
+              {
+                "lightness": 700
+              }
+            ]
+          },
+          {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+              {
+                "color": "#7dcdcd"
+              }
+            ]
+          },
+          {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+          }
+        ]
+      });
+
+      var strictBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(49.025, 2.055),
+        new google.maps.LatLng(49.045, 2.075)
+      );
+
+      // Écouter l'événement de fin de déplacement de la carte
+      google.maps.event.addListener(map, 'dragend', function () {
+        if (strictBounds.contains(map.getCenter())) return;
+
+        // La carte est en dehors des limites - Replacer la carte dans les limites
+        var c = map.getCenter(),
+            x = c.lng(),
+            y = c.lat(),
+            maxX = strictBounds.getNorthEast().lng(),
+            maxY = strictBounds.getNorthEast().lat(),
+            minX = strictBounds.getSouthWest().lng(),
+            minY = strictBounds.getSouthWest().lat();
+
+        if (x < minX) x = minX;
+        if (x > maxX) x = maxX;
+        if (y < minY) y = minY;
+        if (y > maxY) y = maxY;
+
+        // Recentrer la carte dans les limites
+        map.setCenter(new google.maps.LatLng(y, x));
+      });
+    }
+  </script>
 
 </body>
 
